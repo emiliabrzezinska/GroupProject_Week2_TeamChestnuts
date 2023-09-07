af7b6d78d7bd56d738dbbc3836166c83eb7ac040
Bartłomoiej Milewski <bartlomiej.milewski@kainos.com>
2023-09-07 17:03:37 +0200

    Refactor

--- KApi/src/main/java/org/kainos/ea/api/EmployeeService.java
+++ KApi/src/main/java/org/kainos/ea/api/EmployeeService.java
@@ -26,136 +26,113 @@ public class git EmployeeService {
 
             throw new FailedToGetEmployeeListException();
         }
+    }
 
-        public List<Employee> getAllDeliveryEmployees () throws FailedToGetDeliveryEmployeeListException {
-            try {
-                List<Employee> deliveryEmployeeList = employeeDao.getAllDeliveryEmployees();
+    public List<Employee> getAllDeliveryEmployees() throws FailedToGetDeliveryEmployeeListException {
+        try {
+            List<Employee> deliveryEmployeeList = employeeDao.getAllDeliveryEmployees();
 
-                return deliveryEmployeeList;
-            } catch (SQLException e) {
-                System.err.println(e.getMessage());
+            return deliveryEmployeeList;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
 
-                throw new FailedToGetDeliveryEmployeeListException();
+            throw new FailedToGetDeliveryEmployeeListException();
+        }
+    }
+
+    public List<Employee> getAllSalesEmployees() throws FailedToGetSalesEmployeeListException {
+        try {
+            List<Employee> salesEmployeeList = employeeDao.getAllSalesEmployees();
+
+            return salesEmployeeList;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+
+            throw new FailedToGetSalesEmployeeListException();
+        }
+    }
+
+    public Employee getEmployeeById(int, employeeId) throws FailedToGetEmployeeByIdException {
+        try {
+            Employee employee = employeeDao.getEmployeeById(id);
+
+            if (employee == null) {
+                throw new FailedToGetEmployeeByIdException();
             }
+
+            return employee;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+
+            throw new FailedToGetEmployeeByIdException();
+        }
+    }
+
+    public DeliveryEmployee getDeliveryEmployeeById(int, deliveryEmployeeId)
+
+    FailedToGetDeliveryEmployeeByIdException {
+        try {
+            DeliveryEmployee deliveryEmployee = employeeDao.getDeliveryEmployeeById(id);
+
+            if (deliveryEmployee == null) {
+                throw new FailedToGetDeliveryEmployeeByIdException();
+            }
+
+            return deliveryEmployee;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+
+            throw new FailedToGetDeliveryEmployeeByIdException();
+        }
+    }
+
+    public SalesEmployee getSalesEmployeeById(int, salesEmployeeId) throws
+            FailedToGetSalesEmployeeByIdException {
+        try {
+            SalesEmployee salesEmployee = employeeDao.getSalesEmployeeById(id);
+
+            if (salesEmployee == null) {
+                throw new FailedToGetSalesEmployeeByIdException();
+            }
+
+            return salesEmployee;
+        } catch (FailedToGetSalesEmployeeByIdException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateEmployee(int, employeeId, EmployeeRequest employee) throws
+            FailedToUpdateEmployeeException {
+        try {
+            Employee employeeToUpdate = employeeDao.getEmployeeById(id);
+
+            if (employeeToUpdate == null) {
+                throw new EmployeeDoesNotExistException();
+            }
+
+            employeeDao.updateEmployee(id, employee);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+
+            throw new FailedToUpdateEmployeeException();
         }
 
-        public List<Employee> getAllSalesEmployees () throws FailedToGetSalesEmployeeListException {
-            try {
-                List<Employee> salesEmployeeList = employeeDao.getAllSalesEmployees();
+    }
 
-                return salesEmployeeList;
-            } catch (SQLException e) {
-                System.err.println(e.getMessage());
+    public void deleteEmployee(int, employeeId) throws FailedToDeleteEmployeeException {
+        try {
+            Employee employeeToDelete = employeeDao.getEmployeeById(id);
 
-                throw new FailedToGetSalesEmployeeListException();
+            if (employeeToDelete == null) ;
+            {
+                throw new FailedToDeleteEmployeeException();
             }
+            employeeDao.updateEmployee(id, employee);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
 
-            public Employee getEmployeeById ( int employeeId) throws FailedToGetEmployeeByIdException {
-                try {
-                    Employee employee = employeeDao.getEmployeeById(id);
-
-                    if (employee == null) {
-                        throw new FailedToGetEmployeeByIdException();
-                    }
-
-                    return employee;
-                } catch (SQLException e) {
-                    System.err.println(e.getMessage());
-
-                    throw new FailedToGetEmployeeByIdException();
-                }
-
-                public DeliveryEmployee getDeliveryEmployeeById ( int deliveryEmployeeId)
-                FailedToGetDeliveryEmployeeByIdException {
-                    try {
-                        DeliveryEmployee deliveryEmployee = employeeDao.getDeliveryEmployeeById(id);
-
-                        if (deliveryEmployee == null) {
-                            throw new FailedToGetDeliveryEmployeeByIdException();
-                        }
-
-                        return deliveryEmployee;
-                    } catch (SQLException e) {
-                        System.err.println(e.getMessage());
-
-                        throw new FailedToGetDeliveryEmployeeByIdException();
-                    }
-
-                    public SalesEmployee getSalesEmployeeById ( int salesEmployeeId) throws
-                    FailedToGetSalesEmployeeByIdException {
-                        try {
-                            SalesEmployee salesEmployee = employeeDao.getSalesEmployeeById(id);
-
-                            if (salesEmployee == null) {
-                                throw new FailedToGetSalesEmployeeByIdException();
-                            }
-
-                            return salesEmployee;
-                        } catch (FailedToGetSalesEmployeeByIdException e) {
-                            throw new RuntimeException(e);
-                        }
-
-                        public void updateEmployee ( int employeeId, EmployeeRequest employee) throws
-                        FailedToUpdateEmployeeException {
-                            try {
-                                Employee employeeToUpdate = employeeDao.getEmployeeById(id);
-
-                                if (employeeToUpdate == null) {
-                                    throw new EmployeeDoesNotExistException();
-                                }
-
-                                employeeDao.updateEmployee(id, employee);
-                            } catch (SQLException e) {
-                                System.err.println(e.getMessage());
-
-                                throw new FailedToUpdateEmployeeException();
-                            }
-
-                        }
-//NOT NEEDED
-//    public void updateDeliveryEmployee(int id) {
-//        DeliveryEmployee deliveryEmployeeToUpdate = employeeDao.getEmployeeById(id);
-//
-//    }
-//
-//    public void updateSalesEmployee(int id) {
-//        SalesEmployee salesEmployeeToUpdate = employeeDao.getEmployeeById(id);
-//
-//    }
-//NOT NEEDED
-                        public void deleteEmployee ( int id) throws FailedToDeleteEmployeeException {
-                            try {
-                                Employee employeeToDelete = employeeDao.getEmployeeById(id);
-
-                                if (employeeToDelete == null) ;
-                                {
-                                    throw new FailedToDeleteEmployeeException();
-                                }
-                                employeeDao.updateEmployee(id, employee);
-                            } catch (SQLException e) {
-                                System.err.println(e.getMessage());
-
-                                throw new FailedToDeleteEmployeeException();
-                            }
-
-                        }
-//NOT NEEDED
-//                        public int createDeliveryEmployee (Employee deliveryEmployee){
-//                            int id = employeeDao.createDeliveryEmployee(deliveryEmployee);
-//
-//                            return id;
-//                        }
-//
-//                        public int createSalesEmployee (Employee salesEmployee){
-//                            int id = employeeDao.createSalesEmployee(salesEmployee);
-//
-//                            return id;
-//        NOTNEEDED
-                        }
-
-                    }
-                }
-            }
+            throw new FailedToDeleteEmployeeException();
         }
+
     }
 }
